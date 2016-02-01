26d17def4051df40cd474dc349cf9e6acb693b90
Parakh <parakhbansalpbt@gmail.com>
2016-02-02 01:39:27 +0530

    13.Dijkstra's Algorithm

---
+++ dijkstra.cpp
@@ -0,0 +1,143 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define sf scanf
+#define pf printf
+long long int binarySearch(long long int arr[],long long  int l, long long int r, long long int x)
+{
+  while (l <= r)
+  {
+    long long int m = l + (r-l)/2;
+
+    if (arr[m] == x) return m;  // Check if x is present at mid
+
+    if (arr[m] < x) l = m + 1; // If x greater, ignore left half
+
+    else r = m - 1; // If x is smaller, ignore left half
+  }
+  return -1; // if we reach here, then element was not present
+}
+void merge(long long int arr[], long long int l, long long int m, long long int r)
+{
+    long long int i, j, k;
+    long long int n1 = m - l + 1;
+    long long int n2 =  r - m;
+
+    /* create temp arrays */
+   long long  int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for(i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for(j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+/* l is for left index and r is right index of the sub-array
+  of arr to be sorted */
+void mergeSort(long long int arr[], long long int l, long long int r)
+{
+    if (l < r)
+    {
+        long long  int m = l+(r-l)/2;//Same as (l+r)/2, but avoids overflow for large l and h
+        mergeSort(arr, l, m);
+	    mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+}
+void printArray(long long int A[], long long int size)
+{
+   long long  int i;
+    for (i=0; i < size; i++)
+        printf("%lld ", A[i]);
+    printf("\n");
+}
+#define V 9
+int minDistance(int dist[], bool sptSet[])
+{
+   int min = INT_MAX, min_index;
+ 
+   for (int v = 0; v < V; v++)
+     if (sptSet[v] == false && dist[v] <= min)
+         min = dist[v], min_index = v;
+ 
+   return min_index;
+}
+int printSolution(int dist[], int n)
+{
+   printf("Vertex   Distance from Source\n");
+   for (int i = 0; i < V; i++)
+      printf("%d \t\t %d\n", i, dist[i]);
+}
+void dijkstra(int graph[V][V], int src)
+{
+     int dist[V];
+	 bool sptSet[V];
+     for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+ dist[src] = 0;
+ 
+     for (int count = 0; count < V-1; count++)
+     {
+       int u = minDistance(dist, sptSet);
+       sptSet[u] = true;
+       for (int v = 0; v < V; v++)
+         if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
+                                       && dist[u]+graph[u][v] < dist[v])
+            dist[v] = dist[u] + graph[u][v];
+     }
+     printSolution(dist, V);
+}
+int main()
+{
+   int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                      {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                      {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                      {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                      {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                      {0, 0, 4, 0, 10, 0, 2, 0, 0},
+                      {0, 0, 0, 14, 0, 2, 0, 1, 6},
+                      {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                      {0, 0, 2, 0, 0, 0, 6, 7, 0}
+                     };
+ 
+    dijkstra(graph, 0);
+ 
+    return 0;
+}
+
