c4f3a460e743dd29b5bcc7d36f48ea30feb0320d
Parakh <parakhbansalpbt@gmail.com>
2016-02-08 01:33:57 +0530

    Majority Element

---
+++ C++/majority_element.cpp
@@ -0,0 +1,149 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define sf scanf
+#define pf printf
+long long int binarySearch(long long int arr[],long long  int l, long long int r, long long int x)
+{
+  while (l <= r)
+  {
+    long long int m = l + (r-l)/2;
+
+    if (arr[m] == x) return m;  // Check if x is present at mid
+
+    if (arr[m] < x) l = m + 1; // If x greater, ignore left half
+
+    else r = m - 1; // If x is smaller, ignore left half
+  }
+  return -1; // if we reach here, then element was not present
+}
+void merge(long long int arr[], long long int l, long long int m, long long int r)
+{
+    long long int i, j, k;
+    long long int n1 = m - l + 1;
+    long long int n2 =  r - m;
+
+    /* create temp arrays */
+   long long  int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for(i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for(j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+/* l is for left index and r is right index of the sub-array
+  of arr to be sorted */
+void mergeSort(long long int arr[], long long int l, long long int r)
+{
+    if (l < r)
+    {
+        long long  int m = l+(r-l)/2;//Same as (l+r)/2, but avoids overflow for large l and h
+        mergeSort(arr, l, m);
+	    mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+}
+void printArray(long long int A[], long long int size)
+{
+   long long  int i;
+    for (i=0; i < size; i++)
+        printf("%lld ", A[i]);
+    printf("\n");
+}
+
+# define bool int
+  
+int findCandidate(int *, int);
+bool isMajority(int *, int, int);
+  
+/* Function to print Majority Element */
+void printMajority(int a[], int size)
+{
+  /* Find the candidate for Majority*/
+  int cand = findCandidate(a, size);
+  
+  /* Print the candidate if it is Majority*/
+  if(isMajority(a, size, cand))
+    printf(" %d ", cand);
+  else
+    printf("NO Majority Element");
+}
+  
+/* Function to find the candidate for Majority */
+int findCandidate(int a[], int size)
+{
+    int maj_index = 0, count = 1;
+    int i;
+    for(i = 1; i < size; i++)
+    {
+        if(a[maj_index] == a[i])
+            count++;
+        else
+            count--;
+        if(count == 0)
+        {
+            maj_index = i;
+            count = 1;
+        }
+    }
+    return a[maj_index];
+}
+  
+/* Function to check if the candidate occurs more than n/2 times */
+bool isMajority(int a[], int size, int cand)
+{
+    int i, count = 0;
+    for (i = 0; i < size; i++)
+      if(a[i] == cand)
+         count++;
+    if (count > size/2)
+       return 1;
+    else
+       return 0;
+}
+  
+/* Driver function to test above functions */
+int main()
+{
+    int a[] = {1, 3, 3, 1, 2};
+    printMajority(a, 5);
+    getchar();
+    return 0;
+}
