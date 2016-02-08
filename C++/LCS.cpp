ef8cb43ea5d8d2734300324da9608dce43f32292
Parakh <parakhbansalpbt@gmail.com>
2016-02-09 01:19:50 +0530

    Longest Common Subsequence

---
+++ C++/LCS.cpp
@@ -0,0 +1,138 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define sf scanf
+#define pf printf
+long long int binarySearch(long long int arr[],long long  int l, long long int r, long long int x)
+{
+  while (l <= r)
+  {
+    long long int m = l + (r-l)/2;
+
+    if (arr[m] == x) return m;  // Check if x is present at mid
+
+    if (arr[m] < x) l = m + 1; // If x greater, ignore left half
+
+    else r = m - 1; // If x is smaller, ignore left half
+  }
+  return -1; // if we reach here, then element was not present
+}
+void merge(long long int arr[], long long int l, long long int m, long long int r)
+{
+    long long int i, j, k;
+    long long int n1 = m - l + 1;
+    long long int n2 =  r - m;
+
+    /* create temp arrays */
+   long long  int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for(i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for(j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+/* l is for left index and r is right index of the sub-array
+  of arr to be sorted */
+void mergeSort(long long int arr[], long long int l, long long int r)
+{
+    if (l < r)
+    {
+        long long  int m = l+(r-l)/2;//Same as (l+r)/2, but avoids overflow for large l and h
+        mergeSort(arr, l, m);
+	    mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+}
+void printArray(long long int A[], long long int size)
+{
+   long long  int i;
+    for (i=0; i < size; i++)
+        printf("%lld ", A[i]);
+    printf("\n");
+}
+  
+int max(int a, int b);
+  
+/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
+int lcs( char *X, char *Y, int m, int n )
+{
+   int L[m+1][n+1];
+   int i, j;
+  
+   /* Following steps build L[m+1][n+1] in bottom up fashion. Note 
+      that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
+   for (i=0; i<=m; i++)
+   {
+     for (j=0; j<=n; j++)
+     {
+       if (i == 0 || j == 0)
+         L[i][j] = 0;
+  
+       else if (X[i-1] == Y[j-1])
+         L[i][j] = L[i-1][j-1] + 1;
+  
+       else
+         L[i][j] = max(L[i-1][j], L[i][j-1]);
+     }
+   }
+    
+   /* L[m][n] contains length of LCS for X[0..n-1] and Y[0..m-1] */
+   return L[m][n];
+}
+  
+/* Utility function to get max of 2 integers */
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+  
+/* Driver program to test above function */
+int main()
+{
+  char X[] = "AGGTAB";
+  char Y[] = "GXTXAYB";
+  
+  int m = strlen(X);
+  int n = strlen(Y);
+  
+  printf("Length of LCS is %d\n", lcs( X, Y, m, n ) );
+ 
+  return 0;
+}
